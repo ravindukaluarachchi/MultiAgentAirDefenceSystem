3bf053b26f2c05846f05f5b1f5623c869b3abb91
Ravindu Kaluarachchi <raoffire@yahoo.com>
2018-08-08 23:08:43 +0530

    added some test sprites.

--- src/multiagentairdefence/MultiagentAirDefence.java
+++ src/multiagentairdefence/MultiagentAirDefence.java
@@ -12,18 +12,38 @@ import com.rav.agents.RadarAgent;
 import com.rav.agents.SirenAgent;
 import com.rav.environment.Sky;
 import com.rav.util.Position;
+import java.io.File;
+import java.io.FileInputStream;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.JFrame;
 import madkit.kernel.Madkit;
 
 /**
  *
- * @author ravklk
+ * @author ravindu
  */
-public class MultiagentAirDefence {
+public class MultiagentAirDefence extends Application {
+
+    static GraphicsContext gc;
+    static String[] sargs;
+    static Image imgPlane;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        sargs = args;
+        launch(args);
+
+    }
+
+    static void runMAS() {
         Aircraft a = new Aircraft();
         a.setPosition(new Position(0, 0));
         a.setDestination(new Position(1000, 1000));
@@ -31,12 +51,38 @@ public class MultiagentAirDefence {
         Sky.getObjects().add(a);
         a.fly();
         System.out.println("put a on sky");
-        args = new String[]{"--launchAgents",
-              RadarAgent.class.getName() + ",false,1;"
+        sargs = new String[]{"--launchAgents",
+            RadarAgent.class.getName() + ",false,1;"
             + SirenAgent.class.getName() + ",false,1;"
-            +PowerGridAgent.class.getName() + ",false,1;"
-            +AntiAircraftAgent.class.getName() + ",false,1;"};
-        Madkit.main(args);
+            + PowerGridAgent.class.getName() + ",false,1;"
+            + AntiAircraftAgent.class.getName() + ",false,1;"};
+        Madkit.main(sargs);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        File imageFile = new File("images/plane1.jpg");
+        Canvas canvas = new Canvas(800, 600);
+
+        gc = canvas.getGraphicsContext2D();
+
+        StackPane root = new StackPane();
+        root.getChildren().add(canvas);
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Multi Agent Air Defence");
+        primaryStage.setScene(scene);
+
+        imgPlane = new Image(new FileInputStream(imageFile));
+
+        primaryStage.show();
+
+        runMAS();
+    }
+
+    public static void movePlane(Position position) {
+        gc.drawImage(imgPlane, position.getX(), position.getY(), 100, 80);
     }
 
 }
